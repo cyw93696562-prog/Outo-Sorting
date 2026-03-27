4908a0341875d18e3fda7b458f477f14eaf23d00
cyw93696562-prog <cyw93696562@gmail.com>
2026-03-27 13:47:59 +0900

    Adjust padding and height in dashboard styles

--- dashboard.py
+++ dashboard.py
@@ -29,7 +29,7 @@ html, body, [class*="css"] {{
 }}
 
 .block-container {{
-    padding-top: 2000px;
+    padding-top: 150px;
     padding-bottom: 1.2rem;
     max-width: 100%;
     padding-left: 20px;
@@ -41,7 +41,7 @@ html, body, [class*="css"] {{
     top: 0;
     left: 0;
     width: 100%;
-    height: 150px;
+    height: 200px;
     background-color: #ffffff;
     display: flex;
     align-items: center;
